7ddb15afdb322cea7168f5a04e46a501ad5363fb
Jules Dourlens <jdourlens@localhost.localdomain>
2020-10-24 22:33:02 +0200

    Nifty Lottery v1

---
+++ packages/buidler/scripts/deploy_lottery.js
@@ -0,0 +1,182 @@
+const fs = require("fs");
+const chalk = require("chalk");
+const { config, ethers } = require("@nomiclabs/buidler");
+
+async function main() {
+  console.log("📡 Deploy \n");
+  // auto deploy to read contract directory and deploy them all (add ".args" files for arguments)
+  // await autoDeploy();
+  // OR
+  // custom deploy (to use deployed addresses)
+
+  const MuseToken = await deploy("MuseToken");
+  const VNFT = await deploy("VNFT", [MuseToken.address]);
+
+  const Lottery = await deploy("NiftyLottery", [VNFT.address, MuseToken.address]);
+
+  await ethers.provider.send("evm_increaseTime", [60 * 60 *24])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+
+  
+  await VNFT.grantRole(
+    "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6",
+    Lottery.address
+  );
+  console.log("🚀 Granted VNFT Minter Role to Lottery \n");
+
+  // grant miner role to VNFT
+  await MuseToken.grantRole(
+    "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6",
+    VNFT.address
+  );
+  console.log("🚀 Granted MuseToken Minter Role to VNFT \n");
+
+  await MuseToken.mint("0xc783df8a850f42e7f7e57013759c285caa701eb6", "10000000000000000000000");
+
+  // reate an item with 5 points
+  const threeDays = 60 * 60 * 24 * 3;
+  await VNFT.createItem("diamond", 1, 100, threeDays);
+  await VNFT.createItem("cheat", 1, 10000, 60 * 60 * 24);
+  await VNFT.createItem("cheat", 1, 10000, threeDays);
+  console.log("🚀 added item diamond \n");
+
+  await VNFT.mint("0xc783df8a850f42e7F7e57013759C285caa701eB6");
+  await VNFT.mint("0xc783df8a850f42e7F7e57013759C285caa701eB6");
+  await VNFT.mint("0xc783df8a850f42e7F7e57013759C285caa701eB6");
+
+  console.log("🚀 Minted one vNFT to for test \n");
+
+    await Lottery.startLottery(2, 6);
+     infos = await Lottery.getInfos()
+    console.log(infos)
+    await MuseToken.approve(Lottery.address, "10000000000000000000000")
+
+
+
+    await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+
+
+    await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+
+    await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+
+     infos = await Lottery.getInfos()
+    console.log(infos)
+
+    await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+     infos = await Lottery.getInfos()
+    console.log(infos)
+
+    await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ await ethers.provider.send("evm_increaseTime", [60 * 60 *24 + 2])   // add 1day 
+  await ethers.provider.send("evm_mine")      // mine the next block
+  await Lottery.buyTicket("0xc783df8a850f42e7f7e57013759c285caa701eb6")
+  infos = await Lottery.getInfos()
+ console.log(infos)
+ 
+
+
+  // mint to other user to test erc1155 works
+
+  
+
+}
+
+async function deploy(name, _args) {
+  const args = _args || [];
+
+  console.log(`📄 ${name}`);
+  const contractArtifacts = await ethers.getContractFactory(name);
+  const contract = await contractArtifacts.deploy(...args);
+  console.log(
+    chalk.cyan(name),
+    "deployed to:",
+    chalk.magenta(contract.address)
+  );
+  fs.writeFileSync(`artifacts/${name}.address`, contract.address);
+  console.log("\n");
+  return contract;
+}
+
+const isSolidity = (fileName) =>
+  fileName.indexOf(".sol") >= 0 && fileName.indexOf(".swp.") < 0;
+
+function readArgumentsFile(contractName) {
+  let args = [];
+  try {
+    const argsFile = `./contracts/${contractName}.args`;
+    if (fs.existsSync(argsFile)) {
+      args = JSON.parse(fs.readFileSync(argsFile));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  return args;
+}
+
+async function autoDeploy() {
+  let contractList = fs.readdirSync(config.paths.sources);
+  return contractList
+    .filter((fileName) => isSolidity(fileName))
+    .reduce((lastDeployment, fileName) => {
+      const contractName = fileName.replace(".sol", "");
+      const args = readArgumentsFile(contractName);
+
+      // Wait for last deployment to complete before starting the next
+      return lastDeployment.then((resultArrSoFar) =>
+        deploy(contractName, args).then((result) => [...resultArrSoFar, result])
+      );
+    }, Promise.resolve([]));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
